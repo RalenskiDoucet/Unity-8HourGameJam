b2042fe247ed45a9b2bdcc99ed1e9885deab4b14
Ralenski Doucet <s178029@students.aie.edu.au>
2019-05-20 17:32:05 -0500

    8 hour game jam

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public GameObject Player;
+    public CharacterContrller Contrller;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
